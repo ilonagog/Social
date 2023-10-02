12df01d11ea952ad78748919c50209907c4ac769
ilona <gogiashvili.ilona@gmail.com>
2023-10-02 10:06:19 -0400

    created user routes and actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
             render json: {error: "Not Authorized"}
         end
     end
+
+    def create
+        user = User.create!(user_params)
+        if user.valid?
+            session[:user_id]= user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors_full_messages}, status: :unprocessable_entiry
+        end
+    end
 end
